e22d3eb22d6dfa9cfaae707bc8e7c64bc82812d6
coolcrazydog <fan.gao1989@gmail.com>
2019-03-07 16:26:59 -0500

    adding validation(required/email) on input field. add one/two way binding to ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // ngForm could be get with 2 ways, ViewChild or passed in from HTML
+  // (commented out the other way, not is using ViewChild)
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer: string;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
